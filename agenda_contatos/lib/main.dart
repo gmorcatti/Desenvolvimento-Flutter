c99d969c9a32d5ad131b75829eae658a874bd2fc
gmorcatti <gabriel.morcatti@yahoo.com.br>
2020-06-19 19:29:29 -0300

    Arquivo Main alterado

--- agenda_contatos/lib/main.dart
+++ agenda_contatos/lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:agenda_contatos/ui/Contact_Page.dart';
 import 'package:flutter/material.dart';
 import 'package:agenda_contatos/ui/Home_page.dart';
+import 'package:agenda_contatos/ui/Contact_Page.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
